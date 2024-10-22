b94d7de727a7a0e979a9d119193f96a1172bf174
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-10-22 18:50:12 -0500

    Clean up the add deal form

--- src/clj/io/cvcf/payday/web/views/components.clj
+++ src/clj/io/cvcf/payday/web/views/components.clj
@@ -2,20 +2,53 @@
   (:require
    [clojure.string :as s]))
 
-(defn id [n] (format "#%s" n))
+(defn id [n] (format "#%s" (s/replace n #"#" "")))
+(defn attributes [defaults & extras] (apply merge defaults extras))
+(defn classes->str [defaults & extras] (s/join " " (set (apply conj defaults (flatten extras)))))
 
-(defn select-box [label values & {:keys [target-id endpoint selected] :as opts}]
-  [:div
-   [:label.label label]
+(defmacro field [label & body]
+  `[:div.field
+    [:label.label ~label]
+    [:div.control ~@body]])
+
+(defmacro field-group [label & body]
+  `[:div.field
+    [:label.label.subtitle.is-4 ~label]
+    ~@body])
+
+(defn input [name & {:keys [eid type  placeholder required? classes nothing? extra]
+                     :or   {eid         (s/replace name #"\s+" "-")
+                            type        "text"
+                            placeholder (s/capitalize name)
+                            required?   true
+                            classes     ["input"]}}]
+  [:input
+   (attributes
+    {:id (id eid) :type type :name name}
+    (when placeholder {:placeholder placeholder})
+    (when required?   {:required    required?})
+    (when classes     {:class       (classes->str ["input"] classes)})
+    extra)])
+
+(defn select [label values & {:keys [target-id endpoint selected]}]
+  [:div.select
+   (let [n (s/lower-case label)]
+     (into [] (concat [:select (attributes {:id n :name n}
+                                           {:hx-target (id target-id) :hx-get endpoint :hx-swap "outerHTML"})
+                       [:option {:selected (= selected label)} label]]
+                      (map #(vec [:option {:value %1 :selected (= selected %1)} %1]) values))))])
+
+(defn checkbox [label]
+  (let [label (s/trim label)
+        name  (-> label s/lower-case (s/escape {\? "" \  "-"}))]
+    [:div.field
+     [:div.control
+      [:label.checkbox (input name :type "checkbox" :placeholder nil :classes nil :required? nil)
+       (format " %s" label)]]]))
+
+(defn button [label & {:keys [type classes]
+                       :or   {type    "button"
+                              classes ["button"]}}]
+  [:div.field
    [:div.control
-    [:div.select
-     (letfn [(attributes [attrs]
-               (merge attrs
-                      (when target-id
-                        {:hx-target (id target-id)
-                         :hx-get    endpoint
-                         :hx-swap   "outerHTML"})))]
-       (let [n (s/lower-case label)]
-         (into [] (concat [:select (attributes {:id n :name n})
-                           [:option {:selected (= selected label)} label]]
-                          (map #(vec [:option {:value %1 :selected (= selected %1)} %1]) values)))))]]])
+    [:input {:class (classes->str ["button"] classes) :type type :value label}]]])
--- src/clj/io/cvcf/payday/web/views/deals.clj
+++ src/clj/io/cvcf/payday/web/views/deals.clj
@@ -24,164 +24,74 @@
   (let [selected (keyword selected)
         minimum (get-in deal-types [selected :min-down])
         maximum (get-in deal-types [selected :total])]
-    [:input#down-payment.input
-     {:type "number"
-      :name "down-payment"
-      :placeholder "e.g. 1234"
-      :step "0.01"
-      :min minimum
-      :max maximum
-      :required true}]))
+    (c/input "down-payment"
+             :type "number"
+             :placeholder "e.g. 1234.56"
+             :extra {:step "0.01"
+                     :min minimum
+                     :max maximum})))
 
 (defcomponent ^:endpoint new-deal [req target-id endpoint]
   [:div.column
    [:h3.title.is-3 "New deal"]
-   [:form {:id id
-           :hx-post   endpoint
-           :hx-target (c/id target-id)
-           :hx-swap   "outerHTML"}
-    [:div.field
-     [:label.label.subtitle.is-4 "Date"]
-     [:input.input {:type "date"
-                    :name "date"
-                    :required true}]]
+   [:form {:id id :hx-post endpoint :hx-target (c/id target-id) :hx-swap "outerHTML"}
+    (c/field "Date"
+      (c/input "date" :type "date" :placeholder nil))
 
-    [:div.field
-     [:label.label.subtitle.is-4 "Guest Information"]
-     [:div.field
-      [:label.label "Customer Name"]
-      [:div.control
-       [:input.input {:type "text"
-                      :name "name"
-                      :placeholder "e.g. John Smith"
-                      :required true}]]]
+    (c/field-group "Guest Information"
+      (c/field "Full Name" (c/input "name" :placeholder "e.g. John Smith")))
 
-     [:div.field
-      [:label.label.subtitle.is-5 "Contact Information"]
-      [:div.field
-       [:label.label "Email"]
-       [:input.input {:type "email"
-                      :name "email"
-                      :placeholder "e.g. john.smith@email.com"
-                      :required true}]]
+    (c/field-group "Contact Information"
+      (c/field "Email" (c/input "email" :type "email" :placeholder "e.g. john.smith@email.com"))
+      (c/field "Phone" (c/input "phone" :type "phone" :placeholder "e.g. 123.456.7890")))
 
-      [:div.field
-       [:label.label "Phone"]
-       [:input.input {:type "phone"
-                      :name "phone"
-                      :placeholder "e.g. 123.456.7890"}]]]
+    (c/field-group "Address"
+      (c/field "Street"   (c/input "street" :placeholder "e.g. 123 Main St"))
+      (c/field "City"     (c/input "city" :placeholder "e.g. Anycity"))
+      (c/field "State"    (c/input "state" :placeholder "e.g. Anystate"))
+      (c/field "Zip code" (c/input "zip-code" :type "number" :placeholder "e.g. 12345"))
+      (c/field "Country"  (c/input "country" :placeholder "e.g. USA")))
 
-     [:div.field
-      [:label.label.subtitle.is-6 "Address"]
-      [:div.field
-       [:label.label "Street"]
-       [:input.input {:type "text"
-                      :name "street"
-                      :placeholder "e.g. 123 Main St"
-                      :required true}]]
+    (c/field-group "Tour Information"
+      (c/field "Tour type"     (c/input "tour-type" :placeholder "e.g. Monster"))
+      (c/field "Frontline rep" (c/input "frontline-rep" :placeholder "e.g. Jon Bon Jovi")))
 
-      [:div.field
-       [:label.label "City"]
-       [:input.input {:type "text"
-                      :name "city"
-                      :placeholder "e.g. Anycity"
-                      :required true}]]
-
-      [:div.field
-       [:label.label "State"]
-       [:input.input {:type "text"
-                      :name "state"
-                      :placeholder "e.g. Anystate"
-                      :required true}]]
-
-      [:div.field
-       [:label.label "Zip code"]
-       [:input.input {:type "number"
-                      :name "zip"
-                      :placeholder "e.g. 12345"
-                      :required true}]]
-
-      [:div.field
-       [:label.label "Country"]
-       [:input.input {:type "text"
-                      :name "country"
-                      :placeholder "e.g. USA"
-                      :required true}]]]]
-
-    [:div.field
-     [:label.label.subtitle.is-4 "Tour Information"]
-     [:div.field
-      [:div.control
-       [:label.label "Tour type"]
-       [:input.input {:type "input"
-                      :name "tour-type"
-                      :placeholder "e.g. Monster"
-                      :required true}]]]
-     [:div.field
-      [:div.control
-       [:label.label "Frontline rep"]
-       [:input.input {:type "input"
-                      :name "rep"
-                      :placeholder "e.g. Jon Bon Jovi"
-                      :required true}]]]]
-
-    [:div.field
-     [:label.label.subtitle.is-4 "Member Information"]
-     [:div.field
-      [:div.control
-       [:label.label "Contract #"]
-       [:input.input {:type "text"
-                      :name "contract"
-                      :placeholder "e.g. 00065..."
-                      :required true}]]]
-     [:div.field
-      [:div.control
-       [:label.label "Member #"]
-       [:input.input {:type "text"
-                      :name "member"
-                      :placeholder "e.g. 00203..."
-                      :required true}]]]]
+    (c/field-group "Member Information"
+      (c/field "Contract #" (c/input "contract" :placeholder "e.g. 00065..."))
+      (c/field "Member #"   (c/input "member" :placeholder "e.g. 00203...")))
 
     (let [deal-names (keys deal-types)
           values (map name deal-names)
           selected (format "%dk" (apply max (map disco-package->num deal-names)))]
       [:div
-       [:div.field
-        (c/select-box "Deal Type" values
-                      :target-id "down-payment"
-                      :endpoint  "/deals/down-payment"
-                      :selected  selected)]
+       (c/field "Deal type"
+         (c/select "Deal Type" values
+                   :target-id "down-payment"
+                   :endpoint  "/deals/down-payment"
+                   :selected  selected))
 
-       [:div.field
-        [:div.control
-         [:label.label "Down Payment"]
-         (down-payment-input (keyword selected))]]])
+       (c/field "Down payment" (down-payment-input selected))])
 
-    [:div.field
-     [:div.control
-      [:label.label "TO"]
-      [:input.input {:type "text"
-                     :name "to"
-                     :placeholder "e.g. Cameron Diaz"}]]]
+    (c/field "TO"
+      (c/input "to" :placeholder "e.g. Cameron Diaz" :required? false))
 
-    [:div.field
-     [:div.control
-      [:label.checkbox [:input {:type "checkbox" :name "split"}] " Split?"]]]
+    (c/checkbox "Split?")
 
-    [:div.field
-     [:div.control
-      [:input.button.is-primary
-       {:type "submit" :value "Add"}]]]]])
+    (c/button "Add" :type "submit" :classes ["is-primary"])]])
 
 (defcomponent down-payment [req selected]
   (down-payment-input selected))
 
-(defcomponent all [req & {:keys [elt-id]}]
-  [:div.column {:id elt-id}
+(defcomponent all [req & {:keys [eid]}]
+  [:div.column {:id eid}
    [:h3.title.is-3 "Deals"]
-   [:p (:name req)]
-   [:p (:email req)]
-   [:p (:phone req)]])
+   (map #(vec [:p (get req %1)])
+        [;; Guest information
+         :date :name :email :phone :street :city :state :zip-code :country
+         ;; Tour information
+         :tour-type :frontline-rep
+         ;; Deal information
+         :contract :member :deal-type :down-payment :to])])
 
 (defn ->map [m]
   (zipmap (map #(keyword (s/replace %1 #"\s+" "-")) (keys m))
@@ -192,8 +102,8 @@
     [["" (fn [req]
            (page-htmx [:div.columns
                        (new-deal req deals-list-id "/deals/all")
-                       (all req :elt-id deals-list-id)]))]
+                       (all req :eid deals-list-id)]))]
      ["/all" (fn [{:keys [form-params]}]
-               (ui (all (->map form-params) :elt-id deals-list-id)))]
+               (ui (all (->map form-params) :eid deals-list-id)))]
      ["/down-payment" (fn [{:keys [params]}]
                         (ui (down-payment nil (:deal-type (->map params)))))]]))
