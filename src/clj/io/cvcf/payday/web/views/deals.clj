5770531dce232bfd9d82793f1633a84a1b493240
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-10-23 09:13:20 -0500

    Add some more styling around the form

--- src/clj/io/cvcf/payday/web/views/deals.clj
+++ src/clj/io/cvcf/payday/web/views/deals.clj
@@ -20,51 +20,52 @@
 (defcomponent new-deal [req target-id endpoint]
   [:div.column.is-two-fifths
    [:h3.title.is-3 "New deal"]
-   [:form {:id id :hx-post endpoint :hx-target (c/id target-id) :hx-swap "outerHTML"}
-    (c/field "Date"
-      (c/input "date" :type "date" :placeholder nil))
+   [:div.box
+    [:form {:id id :hx-post endpoint :hx-target (c/id target-id) :hx-swap "outerHTML"}
+     (c/field "Date" (c/input "date" :type "date" :placeholder nil))
 
-    (c/field-group "Guest Information"
-      (c/field "Full Name" (c/input "name" :placeholder "e.g. John Smith")))
+     (c/field-group "Guest Information"
+       (c/field "Full Name" (c/input "name" :placeholder "e.g. John Smith")))
 
-    (c/field-group "Contact Information"
-      (c/field "Email" (c/input "email" :type "email" :placeholder "e.g. john.smith@email.com"))
-      (c/field "Phone" (c/input "phone" :type "phone" :placeholder "e.g. 123.456.7890")))
+     (c/field-group "Contact Information"
+       (c/field "Email" (c/input "email" :type "email" :placeholder "e.g. john.smith@email.com"))
+       (c/field "Phone" (c/input "phone" :type "phone" :placeholder "e.g. 123.456.7890"))
 
-    (c/field-group "Address"
-      (c/field "Street"   (c/input "street" :placeholder "e.g. 123 Main St"))
-      (c/field "City"     (c/input "city" :placeholder "e.g. Anycity"))
-      (c/field "State"    (c/input "state" :placeholder "e.g. Anystate"))
-      (c/field "Zip code" (c/input "zip-code" :type "number" :placeholder "e.g. 12345"))
-      (c/field "Country"  (c/input "country" :placeholder "e.g. USA")))
+       (c/field-group "Address"
+         (c/field "Street"   (c/input "street" :placeholder "e.g. 123 Main St"))
+         (c/field "City"     (c/input "city" :placeholder "e.g. Anycity"))
+         (c/field "State"    (c/input "state" :placeholder "e.g. Anystate"))
+         (c/field "Zip code" (c/input "zip-code" :type "number" :placeholder "e.g. 12345"))
+         (c/field "Country"  (c/input "country" :placeholder "e.g. USA"))))
 
-    (c/field-group "Tour Information"
-      (c/field "Tour type"     (c/input "tour-type" :placeholder "e.g. Monster"))
-      (c/field "Frontline rep" (c/input "frontline-rep" :placeholder "e.g. Jon Bon Jovi")))
+     (c/field-group "Tour Information"
+       (c/field "Tour type"     (c/input "tour-type" :placeholder "e.g. Monster"))
+       (c/field "Frontline rep" (c/input "frontline-rep" :placeholder "e.g. Jon Bon Jovi")))
 
-    (c/field-group "Member Information"
-      (c/field "Contract #" (c/input "contract" :placeholder "e.g. 00065..."))
-      (c/field "Member #"   (c/input "member" :placeholder "e.g. 00203...")))
+     (c/field-group "Member Information"
+       (c/field "Contract #" (c/input "contract" :placeholder "e.g. 00065..."))
+       (c/field "Member #"   (c/input "member" :placeholder "e.g. 00203...")))
 
-    (let [deal-names (keys deals/deal-types)
-          values (map name deal-names)
-          selected (format "%dk" (apply max (map deals/disco-package->num deal-names)))]
-      [:div
-       (c/field "Deal type"
-         (c/select "Deal Type" values
-                   :target-id "down-payment"
-                   :endpoint  "/deals/down-payment"
-                   :selected  selected))
+     (c/field-group "Deal Information"
+       (let [deal-names (keys deals/deal-types)
+             values     (map name deal-names)
+             selected   (format "%dk" (apply max (map deals/disco-package->num deal-names)))]
+         [:div
+          (c/field "Deal type"
+            (c/select "Deal Type" values
+                      :target-id "down-payment"
+                      :endpoint  "/deals/down-payment"
+                      :selected  selected))
 
-       (c/field "Down payment" (down-payment nil selected))])
+          (c/field "Down payment" (down-payment nil selected))])
 
-    (c/field "TO"
-      (c/input "to" :placeholder "e.g. Cameron Diaz" :required? false))
+       (c/field "TO"
+         (c/input "to" :placeholder "e.g. Cameron Diaz" :required? false))
 
-    (c/checkbox "Split?")
+       (c/checkbox "Split?")
 
-    [:div.buttons.is-centered
-     (c/button "Add" :type "submit" :classes ["is-primary" "is-medium"])]]])
+       [:div.buttons.is-centered
+        (c/button "Add" :type "submit" :classes ["is-primary" "is-medium"])])]]])
 
 (defcomponent all-deals [req & {:keys [eid]}]
   [:div.column.is-two-fifths {:id eid}
