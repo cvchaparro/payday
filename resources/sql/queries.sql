fc7fc52877a2bbb27d2da59ef8b4184f0015541e
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-10-23 20:42:24 -0500

    Add queries

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,1 +1,71 @@
 -- Place your queries here. Docs available https://www.hugsql.org/
+
+-- :name add-deal! :! :n
+-- :doc Add a new deal
+INSERT INTO deals (
+  date,
+  members,
+  email,
+  phone,
+  second_phone,
+  addr_street,
+  addr_city,
+  addr_state,
+  addr_zip,
+  addr_country,
+
+  tour_type,
+  frontline_rep,
+
+  contract_num,
+  member_num,
+  deal_type,
+  down_payment,
+  turn_over,
+  split
+) values (
+  :date,
+  :members,
+  :email,
+  :phone,
+  :second_phone,
+  :addr_street,
+  :addr_city,
+  :addr_state,
+  :addr_zip,
+  :addr_country,
+
+  :tour_type,
+  :frontline_rep,
+
+  :contract_num,
+  :member_num,
+  :deal_type,
+  :down_payment,
+  :turn_over,
+  :split
+);
+
+
+-- :name remove-deal-by-id! :! :n
+-- :doc Remove a deal by the specified ID
+DELETE FROM deals
+ WHERE
+   id = :id;
+
+
+-- :name remove-deals! :! :n
+-- :doc Remove all deals
+DELETE FROM deals;
+
+
+-- :name get-deal-by-id :? :1
+-- :doc Return the deal with the given ID
+SELECT * FROM deals
+ WHERE
+ id = :id;
+
+
+-- :name get-deals :? :*
+-- :doc Return all the deals
+SELECT * FROM deals;
