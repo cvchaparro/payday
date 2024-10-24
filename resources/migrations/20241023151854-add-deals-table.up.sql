1804024e35094ccead77f8e52497aec910e6977b
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-10-24 12:12:59 -0500

    Modify column types

--- resources/migrations/20241023151854-add-deals-table.up.sql
+++ resources/migrations/20241023151854-add-deals-table.up.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS deals (
   addr_street   VARCHAR(100)  NOT NULL,
   addr_city     VARCHAR(100)  NOT NULL,
   addr_state    VARCHAR(30)   NOT NULL,
-  addr_zip      INTEGER       NOT NULL,
+  addr_zip      VARCHAR(30)   NOT NULL,
   addr_country  VARCHAR(30)   NOT NULL DEFAULT "USA",
 
   tour_type     VARCHAR(30)   NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS deals (
 
   contract_num  VARCHAR(30)   NOT NULL,
   member_num    VARCHAR(30)   NOT NULL,
-  deal_type     INTEGER       NOT NULL,
+  deal_type     VARCHAR(10)   NOT NULL,
   down_payment  REAL          NOT NULL,
   turn_over     VARCHAR(100),
   split         BOOLEAN
