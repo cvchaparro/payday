6bfbcba99d0b1448fd763173f43081a542c2db89
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-10-30 12:05:20 -0500

    Add volume per deal for each deal type

--- src/clj/io/cvcf/payday/web/controllers/deals.clj
+++ src/clj/io/cvcf/payday/web/controllers/deals.clj
@@ -5,11 +5,13 @@
 (def deal-types {:300k {:name "300k"
                         :min-down 349
                         :down-cutoff 1300
-                        :total 3178.5}
+                        :total 3178.5
+                        :volume 2770}
                  :400k {:name "400k"
                         :min-down 449
                         :down-cutoff 1400
-                        :total 4036.5}})
+                        :total 4036.5
+                        :volume 3550}})
 
 (defn disco-package->num [package-name]
   (->> (name package-name)
