88ff152777f711ce299343170c528b64ee411a5f
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-11-05 06:01:46 -0600

    Handle keys that are already keywords

--- src/clj/io/cvcf/payday/helpers.clj
+++ src/clj/io/cvcf/payday/helpers.clj
@@ -2,7 +2,9 @@
   (:require
    [clojure.string :as s]))
 
-(defn ->map [m]
-  (zipmap (map #(if (keyword? %) % (keyword (s/replace % #"\s+" "-")))
+(defn ->map [m & {:keys [key-fn]
+                  :or   {key-fn identity}}]
+  (zipmap (map #(let [n (key-fn %)]
+                  (if (keyword? n) n (keyword (s/replace n #"\s+" "-"))))
                (keys m))
           (vals m)))
