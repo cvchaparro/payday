4b3ea486354db8460a901e59dc075323558d7288
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-11-08 19:15:19 -0600

    Allow modifying values when generating a map

--- src/clj/io/cvcf/payday/helpers.clj
+++ src/clj/io/cvcf/payday/helpers.clj
@@ -2,9 +2,9 @@
   (:require
    [clojure.string :as s]))
 
-(defn ->map [m & {:keys [key-fn]
-                  :or   {key-fn identity}}]
+(defn ->map [m & {:keys [key-fn val-fn]
+                  :or   {key-fn identity val-fn identity}}]
   (zipmap (map #(let [n (key-fn %)]
                   (if (keyword? n) n (keyword (s/replace n #"\s+" "-"))))
                (keys m))
-          (vals m)))
+          (map val-fn (vals m))))
