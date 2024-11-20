f815041464f7a7f7be3bfb6df96edd11c464059c
Cameron Chaparro <cameron+dev@chaparroconsulting.group>
2024-11-19 22:46:20 -0600

    Make the site responsive

--- src/clj/io/cvcf/payday/web/htmx.clj
+++ src/clj/io/cvcf/payday/web/htmx.clj
@@ -13,6 +13,7 @@
   (page
    [:head
     [:meta {:charset "UTF-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
     [:title "Wyndham Payday"]
     [:link {:href "/css/all.min.css" :rel "stylesheet" :type "text/css"}]
     [:script {:src "/js/htmx.min.js" :defer true}]
